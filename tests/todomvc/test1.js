190d4a7ac8a16931d602b06f1ea12dca888c606a
Mohammed Jameel Asfer Zahir Hussain <mohammedjameelasferz@lumel.com>
2025-09-12 16:06:47 +0530

    main

--- tests/todomvc/test1.js
+++ tests/todomvc/test1.js
@@ -3,7 +3,7 @@ const assert = require('assert');
 Feature('ToDo');
 
 Scenario('todo item activities', async({ I }) => {
-  //create an item
+  //test 1 : create an item
   I.amOnPage('https://todomvc.com/examples/react/dist/');
   I.dontSeeElement('.todo-count')
   I.fillField(`//input[@id='todo-input']`, 'Write a guide');
@@ -14,58 +14,53 @@ Scenario('todo item activities', async({ I }) => {
   I.pressKey('Enter');
   I.fillField(`//input[@id='todo-input']`, 'Promote the guide');
   I.pressKey('Enter');
-
   //visibility of items
   I.seeElement(`//li[@data-testid='todo-item']//label[text()='Write a guide']`);
   I.seeElement(`//li[@data-testid='todo-item']//label[text()='Correct the guide']`);
   I.seeElement(`//li[@data-testid='todo-item']//label[text()='Publish the guide']`);
   I.seeElement(`//li[@data-testid='todo-item']//label[text()='Promote the guide']`);
-
   //checking no of items totally
   I.see('4 items left', '.todo-count');
   
-  //complete an item
+  //test 2 : complete an item
   I.moveCursorTo(`//li[@data-testid = 'todo-item']//*[text()='Correct the guide']`);
   I.click(`//li[@data-testid = 'todo-item']//label[text()='Correct the guide']/preceding-sibling::input`);  
   I.seeElement(`//label[text()='Correct the guide']/ancestor::li[@data-testid = 'todo-item'  and @class='completed']`);
-  
   //checking no of items totally
   I.see('3 items left', '.todo-count');
 
-  //delete an item
+  //test 3 : delete an item
   I.moveCursorTo(`//li[@data-testid = 'todo-item']//*[text()='Publish the guide']`);
   I.click(`//li[@data-testid = 'todo-item']//label[text()='Publish the guide']/following-sibling::button`)
   I.dontSeeElement(`//li[@data-testid = 'todo-item']//*[text()='Publish the guide']`)
-  
   //checking no of items totally
   I.see('2 items left', '.todo-count');
 
-  //edit an item
+  //test 4 : edit an item
   I.moveCursorTo(`//li[@data-testid = 'todo-item']//*[text()='Promote the guide']`);
   I.doubleClick(`//li[@data-testid='todo-item']//label[text()='Promote the guide']/parent::div`);
   I.seeElementInDOM(`//li[@data-testid='todo-item']//input[@data-testid='text-input']`);
   I.fillField(`//li[@data-testid='todo-item']//input[@data-testid='text-input']`,'updated');
   I.pressKey('Enter')
-  
   //checking the visibility of edited and removed text of item
   I.seeElement(`//li[@data-testid='todo-item']//label[text()='updated']`);
   I.dontSee(`//li[@data-testid='todo-item']//label[text()='Promote the guide']`);
 
+  //test 5 : asserting the count of elements
   //checking no of items in "all"
   I.click(`//ul[@class='filters']//li//a[@href='#/']`)
   const count1 = await I.grabNumberOfVisibleElements(`//ul[@class='todo-list']//li`)
   assert.strictEqual(count1, 3)
-
   //checking no of items in "active"
   I.click(`//ul[@class='filters']//li//a[@href='#/active']`)
   const count2 = await I.grabNumberOfVisibleElements(`//ul[@class='todo-list']//li`)
   assert.strictEqual(count2, 2)
-
   //checking no of items in "completed"
   I.click(`//ul[@class='filters']//li//a[@href='#/completed']`)
   const count3 = await I.grabNumberOfVisibleElements(`//ul[@class='todo-list']//li`)
   assert.strictEqual(count3, 1)
 
+  //test 6 : checking and unchecking all entries
   //in "all tab"
   //toggling all as checked
   I.click(`//ul[@class='filters']//li//a[@href='#/']`)
@@ -73,36 +68,38 @@ Scenario('todo item activities', async({ I }) => {
   I.click(`//input[@id='toggle-all']`)
   I.seeNumberOfVisibleElements("//li[@data-testid = 'todo-item' and @class='completed']", 3);
   I.see('0 items left', '.todo-count');
-
   //untoggling all
   I.click(`//ul[@class='filters']//li//a[@href='#/']`)
   I.moveCursorTo(`//div[@class='toggle-all-container']`)
   I.click(`//input[@id='toggle-all']`)
   I.see('3 items left', '.todo-count'); 
 
+  //test 7 : checking a single item
   //checking an item
   I.moveCursorTo(`//li[@data-testid = 'todo-item']//*[text()='Write a guide']`);
   I.click(`//li[@data-testid = 'todo-item']//label[text()='Write a guide']/preceding-sibling::input`);  
   I.seeElement(`//label[text()='Write a guide']/ancestor::li[@data-testid = 'todo-item'  and @class='completed']`);
 
+  //test 8: using clear completed
   //clear completed one
   I.click(`//ul[@class='filters']/following-sibling::button[text()='Clear completed']`)
   I.dontSeeElement(`//li[@data-testid = 'todo-item']//*[text()='Write a guide']`)
   I.see('2 items left', '.todo-count');
   
+  //test 9
   //checking all elements
   I.click(`//ul[@class='filters']//li//a[@href='#/']`)
   I.moveCursorTo(`//div[@class='toggle-all-container']`)
   I.click(`//input[@id='toggle-all']`)
   I.seeNumberOfVisibleElements("//li[@data-testid = 'todo-item' and @class='completed']", 2);
   I.seeElement(`//span[text()='0 items left!']`);
-
   //clear completed one
   I.click(`//ul[@class='filters']/following-sibling::button[text()='Clear completed']`)
   I.dontSeeElement(`//li[@data-testid = 'todo-item']//*[text()='Correct the guide']`)
   I.dontSeeElement(`//li[@data-testid = 'todo-item']//*[text()='Publish the guide']`)
   I.dontSeeElement(`//span[text()='0 items left!']`);
 
+  //test 10 : exploring active tab
   //repeating the addition of items
   I.fillField(`//input[@id='todo-input']`, 'Write a guide');
   I.pressKey('Enter');
@@ -112,31 +109,25 @@ Scenario('todo item activities', async({ I }) => {
   I.pressKey('Enter');
   I.fillField(`//input[@id='todo-input']`, 'Promote the guide');
   I.pressKey('Enter');
-
   //repeating visibility of items
   I.seeElement(`//li[@data-testid='todo-item']//label[text()='Write a guide']`);
   I.seeElement(`//li[@data-testid='todo-item']//label[text()='Correct the guide']`);
   I.seeElement(`//li[@data-testid='todo-item']//label[text()='Publish the guide']`);
   I.seeElement(`//li[@data-testid='todo-item']//label[text()='Promote the guide']`);
-
   //in "active" tab
   //checking "clear completed" and "toggle all and untoggle all"
   //toggle all
   I.click(`//ul[@class='filters']//li//a[@href='#/active']`)
-
   //checking the count first
   const countActive = await I.grabNumberOfVisibleElements(`//ul[@class='todo-list']//li`)
   assert.strictEqual(countActive, 4)
-
   //toggle all in active
   I.moveCursorTo(`//div[@class='toggle-all-container']`)
   I.click(`//input[@id='toggle-all']`)
-
   //checking visibility
   I.dontSeeElement(`//li[@data-testid = 'todo-item']`)
   I.seeNumberOfVisibleElements(`//li[@data-testid = 'todo-item' and @class='completed']`, 0);
   I.seeElement(`//span[text()='0 items left!']`);
-
   //checking visibility in all tab
   I.click(`//ul[@class='filters']//li//a[@href='#/']`)
   I.seeNumberOfVisibleElements("//li[@data-testid = 'todo-item' and @class='completed']", 4);
@@ -145,13 +136,12 @@ Scenario('todo item activities', async({ I }) => {
   I.click(`//ul[@class='filters']//li//a[@href='#/completed']`)
   I.seeNumberOfVisibleElements("//li[@data-testid = 'todo-item' and @class='completed']", 4);
   I.see('0 items left', '.todo-count');
-
   //clicking "clear completed"
   I.click(`//ul[@class='filters']/following-sibling::button[text()='Clear completed']`)
   I.dontSeeElement(`//span[text()='0 items left!']`);
 
+  //test 11 : exploring completed tab
   //inserting again
-
   //in "completed" tab
   //checking "clear completed" and "toggle all and untoggle all"
   //toggle all
@@ -159,14 +149,12 @@ Scenario('todo item activities', async({ I }) => {
   I.pressKey('Enter');
   I.fillField(`//input[@id='todo-input']`, 'Correct the guide');
   I.pressKey('Enter');
-
   //checking if completed items exist
   I.click(`//ul[@class='filters']//li//a[@href='#/completed']`)
   I.seeNumberOfVisibleElements("//li[@data-testid = 'todo-item' and @class='completed']", 0);
   I.see('2 items left', '.todo-count');
-
   //toggling the elements in "all" to completed
-  //checking first
+  //checking  visibility first
   I.click(`//ul[@class='filters']//li//a[@href='#/']`)
   I.seeNumberOfVisibleElements("//li[@data-testid = 'todo-item']", 2);
   I.see('2 items left', '.todo-count');
@@ -184,12 +172,12 @@ Scenario('todo item activities', async({ I }) => {
   I.seeNumberOfVisibleElements("//li[@data-testid = 'todo-item' and @class='completed']", 0);
   I.dontSeeElement(`//span[text()='0 items left!']`);
 
+  //test 12 : untoggle
   //exploring untoggle
   I.fillField(`//input[@id='todo-input']`, 'Write a guide');
   I.pressKey('Enter');
   I.fillField(`//input[@id='todo-input']`, 'Correct the guide');
   I.pressKey('Enter');
-
   //checking if completed items exist
   I.click(`//ul[@class='filters']//li//a[@href='#/']`)
   I.seeNumberOfVisibleElements("//li[@data-testid = 'todo-item' and @class='completed']", 0);
@@ -201,7 +189,6 @@ Scenario('todo item activities', async({ I }) => {
   I.click(`//ul[@class='filters']//li//a[@href='#/completed']`)
   I.seeNumberOfVisibleElements("//li[@data-testid = 'todo-item' and @class='completed']", 2);
   I.see('0 items left', '.todo-count');
-
   //coming to all and untoggling
   I.click(`//ul[@class='filters']//li//a[@href='#/']`)
   I.moveCursorTo(`//div[@class='toggle-all-container']`)
@@ -210,6 +197,7 @@ Scenario('todo item activities', async({ I }) => {
   I.see('2 items left', '.todo-count');
   I.seeNumberOfVisibleElements("//li[@data-testid = 'todo-item' and @class='completed']", 0);
 
+  //test 13
   //also via "completed"
   //being in all page
   I.moveCursorTo(`//div[@class='toggle-all-container']`)
@@ -234,6 +222,7 @@ Scenario('todo item activities', async({ I }) => {
   I.seeNumberOfVisibleElements(`//li[@data-testid = 'todo-item']`, 2);
   I.see('2 items left', '.todo-count');
 
+  //test 14
   //also via "active"
   I.click(`//ul[@class='filters']//li//a[@href='#/active']`)
   I.seeNumberOfVisibleElements(`//li[@data-testid = 'todo-item']`, 2);
@@ -256,10 +245,13 @@ Scenario('todo item activities', async({ I }) => {
   I.click(`//ul[@class='filters']//li//a[@href='#/']`)
   I.seeNumberOfVisibleElements(`//li[@data-testid = 'todo-item']`, 2);
   I.see('2 items left', '.todo-count');
+
+
   //last two entries are
   //write a guide
   //correct the guide
 
+  //test 15
   //now completing a specific item
   //complete an item
   I.moveCursorTo(`//li[@data-testid = 'todo-item']//*[text()='Correct the guide']`);
@@ -281,6 +273,7 @@ Scenario('todo item activities', async({ I }) => {
   I.seeElement(`//label[text()='Correct the guide']/ancestor::li[@data-testid = 'todo-item']`);
   I.see('2 items left', '.todo-count');
 
+  //test 16
   //deleting spcific item and verifying its visibility
   //for an active item
   //delete an item in "all" page
@@ -331,6 +324,7 @@ Scenario('todo item activities', async({ I }) => {
   I.dontSeeElement(`//li[@data-testid = 'todo-item']//*[text()='Correct the guide']`)
   I.see('1 item left', '.todo-count');
 
+  //test 17
   //deleting spcific item and verifying its visibility
   //for an completed item
   //delete an item in "all" page
@@ -383,6 +377,7 @@ Scenario('todo item activities', async({ I }) => {
   I.dontSeeElement(`//label[text()='Publish the guide' and @class='completed']/ancestor::li[@data-testid = 'todo-item']`);
   I.see('1 item left', '.todo-count');
 
+  //test 18
   //editing an active element and checking if it is edited in all and active
   I.click(`//ul[@class='filters']//li//a[@href='#/']`)
   I.fillField(`//input[@id='todo-input']`, 'Publish the guide');
@@ -405,6 +400,7 @@ Scenario('todo item activities', async({ I }) => {
   I.see('2 items left', '.todo-count');
 
 
+  //test 19
   //editing an completed element and checking if it is edited in all and active
   I.click(`//ul[@class='filters']//li//a[@href='#/']`)
   I.see('2 items left', '.todo-count');
