151290775d79fe1066adb7330b5a8c504f937516
JameelAsferZ <MohammedJameelAsferZ@lumel.com>
2025-09-18 09:54:12 +0530

    main

--- tests/parabank/tests/test02.js
+++ tests/parabank/tests/test02.js
@@ -7,7 +7,7 @@ Before(async () => {
   await login2Page.open();
 });
 
-Scenario('proper login', async () => {
+Scenario('proper login of page', async () => {
   await login2Page.loginToThePage();
   await login2Page.clickLogin();
   await login2Page.loginResult();  
